355cf8373c2421cde00a0247e7b0302b2e08c93f
jsunh98 <jsunh98@gmail.com>
2023-06-01 11:11:16 -0400

    added styling and javascript. put the box inside of a container class and.

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ let box = document.querySelector('.box');
 box.addEventListener('mousemove', (e) => {
     let x = e.offsetX
     let y = e.offsetY
-    let BoxWidth = box.clientWidth
-    let BoxHeight = box.clientHeight
-    let moveX = (x - BoxWidth)
-    let moveY = (y - BoxHeight)
+    let boxWidth = box.clientWidth
+    let boxHeight = box.clientHeight
+    let moveX = (x - boxWidth/2)
+    let moveY = (y - boxHeight/2)
     box.style.transform = `translateX(${moveX}px) translateY(${moveY}px)`;
 
+})
+box.addEventListener('mouseout', (e) => {
+    box.style.transform = ``
 })
